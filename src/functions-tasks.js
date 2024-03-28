9f3281949c6f0deeab7b46937e93a5f547f0edae
ntsh <106670334+tffl@users.noreply.github.com>
2024-03-28 13:01:40 +0300

    feat: memoize solution

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -107,8 +107,17 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let cached = null;
+  let result = false;
+
+  return function () {
+    if (!result) {
+      cached = func();
+      result = true;
+    }
+    return cached;
+  };
 }
 
 /**
