762990d91ff817917a10a393e0e7e3d8d6d0f5c2
ntsh <106670334+tffl@users.noreply.github.com>
2024-03-28 14:22:15 +0300

    feat: partialUsingArguments solution

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -189,8 +189,8 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return fn.bind(null, ...args1);
 }
 
 /**
